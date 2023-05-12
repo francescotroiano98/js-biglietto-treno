2e8f44770fdd8bfe991bc23fc4fb926b72e688e1
francescotroiano98 <francesco-troiano-98@libero.it>
2023-05-12 14:46:13 +0200

    Done Prezzo biglietto

--- JS/script.js
+++ JS/script.js
@@ -1,2 +1,15 @@
-const kilometri = parseInt( prompt("scrivi il tuo nome"));
+const kilometri = parseInt(prompt("scrivi quanti km farai"));
 console.log(kilometri);
+const age = parseInt(prompt("scrivi la tua età"));
+console.log(age);
+let ticketprice = 0.21 * kilometri;
+console.log(ticketprice);
+
+if (age < 18) {
+    ticketprice = (kilometri * 0.21) - ((kilometri*0.21*20)/100);
+} else if (age > 65) {
+    ticketprice = (kilometri * 0.21) - ((kilometri*0.21*40)/100);
+} else {
+    ticketprice = 0.21 * kilometri;
+}
+document.getElementById("answerPrice").innerHTML = ticketprice;
