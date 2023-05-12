67cf854f820bad33c8b56cda024c43f081bd45aa
francescotroiano98 <francesco-troiano-98@libero.it>
2023-05-12 16:30:34 +0200

    fix ticketPrice

--- JS/script.js
+++ JS/script.js
@@ -7,13 +7,10 @@ console.log (ticketprice);
 
 
     if (age < 18) {
-        ticketprice = (kilometri * 0.21) - ((kilometri*0.21*20)/100);
+        ticketprice = ticketprice - ((kilometri*0.21*20)/100);
     } else if (age > 65) {
-        ticketprice = (kilometri * 0.21) - ((kilometri*0.21*40)/100);
-    } else {
-        ticketprice = 0.21 * kilometri;
-    }
-
+        ticketprice = ticketprice - ((kilometri*0.21*40)/100);
+    } 
 
 
 
