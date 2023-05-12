5c8ea02945509e3ffd2cfb2f92be1ca79eb8b7b9
francescotroiano98 <francesco-troiano-98@libero.it>
2023-05-12 16:03:41 +0200

    add decimal numbers

--- JS/script.js
+++ JS/script.js
@@ -3,13 +3,19 @@ console.log(kilometri);
 const age = parseInt(prompt("scrivi la tua età"));
 console.log(age);
 let ticketprice = 0.21 * kilometri;
-console.log(ticketprice);
+console.log (ticketprice);
 
-if (age < 18) {
-    ticketprice = (kilometri * 0.21) - ((kilometri*0.21*20)/100);
-} else if (age > 65) {
-    ticketprice = (kilometri * 0.21) - ((kilometri*0.21*40)/100);
-} else {
-    ticketprice = 0.21 * kilometri;
-}
-document.getElementById("answerPrice").innerHTML = ticketprice;
+
+    if (age < 18) {
+        ticketprice = (kilometri * 0.21) - ((kilometri*0.21*20)/100);
+    } else if (age > 65) {
+        ticketprice = (kilometri * 0.21) - ((kilometri*0.21*40)/100);
+    } else {
+        ticketprice = 0.21 * kilometri;
+    }
+
+
+
+
+
+document.getElementById("answerPrice").innerHTML = ticketprice.toFixed(2);
