7c0bbdc5c12c5609997f8c25e273431a8be2b073
francescotroiano98 <francesco-troiano-98@libero.it>
2023-05-13 15:11:01 +0200

    Changed solution to a better one

--- JS/script.js
+++ JS/script.js
@@ -5,17 +5,30 @@ console.log(age);
 let ticketprice = 0.21 * kilometri;
 console.log (ticketprice);
 
-if (isNaN(parseInt(kilometri && age))){
-    alert("Hai messo qualcosa che non va")
-} 
+if (isNaN(kilometri) && isNaN(age)) {
+    document.getElementById("error").innerHTML = "Hai inserito un valore non valido per i kilometri e per l'età!";
 
+    alert("Hai inserito un valore non valido per i kilometri e per l'età!");
+
+  } else if (isNaN(kilometri)) {
+    document.getElementById("error").innerHTML = "Hai inserito un valore non valido per i kilometri!";
+
+    alert("Hai inserito un valore non valido per i kilometri!");
+
+  } else if (isNaN(age)) {
+    document.getElementById("error").innerHTML = "Hai inserito un valore non valido per l'età!";
+
+    alert("Hai inserito un valore non valido per l'età!");
+    
+  } else {
+    ticketprice;
+    console.log(ticketprice);
+  
     if (age < 18) {
         ticketprice = ticketprice - ((kilometri*0.21*20)/100);
     } else if (age > 65) {
         ticketprice = ticketprice - ((kilometri*0.21*40)/100);
-    } 
-
-
-
-
-document.getElementById("answerPrice").innerHTML = ticketprice.toFixed(2);
+    }
+  
+    document.getElementById("answerPrice").innerHTML = ticketprice.toFixed(2);
+  }
